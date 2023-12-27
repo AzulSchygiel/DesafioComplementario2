85fe74dbb942c9e822745ba12ebd2061d9548870
AzulSchygiel <132393983+AzulSchygiel@users.noreply.github.com>
2023-12-26 21:34:14 -0300

    Add files via upload

---
+++ src/config/swagger.config.js
@@ -0,0 +1,17 @@
+import __dirname from "../utils/index.js";
+import swaggerJsDoc from "swagger-jsdoc";
+import path from "path";
+
+const swaggerOpciones = {
+    definition:{
+        openapi:"3.0.1",
+        info:{
+            title:"DOCUMENTACIÓN API PARA APP DE MASCOTAS",
+            version:"1.0.0",
+            description:"Endpoints de la API de autenticación."
+        },
+    },
+    apis: [`${path.join(__dirname, "../docs/**/*.yaml")}`],  
+},
+
+export const swaggerSpecs = swaggerJsDoc(swaggerOpciones);
